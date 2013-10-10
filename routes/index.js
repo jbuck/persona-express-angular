ff0a89e6677b0c6ed4ca4e1d97d28c8adb952476
Jennifer Fong <jen@ednapiranha.com>
2013-10-10 15:57:13 -0700

    added comment about /login

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 
 module.exports = function(app, isLoggedIn) {
   app.get('/login', isLoggedIn, function (req, res) {
+    // Persona email is already saved in the session but showing this so that you
+    // can set other session items and return in this payload.
     res.json({
       email: req.session.email
     });
